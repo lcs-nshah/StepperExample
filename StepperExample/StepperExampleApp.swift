96f3f1c7de79a20c2c5584288fcfb91caf3e108d
Neil Shah <nshah@lcs.on.ca>
2025-02-12 08:45:11 -0500

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Neil Shah on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
