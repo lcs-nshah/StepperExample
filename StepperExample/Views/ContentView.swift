68cceb937c82bf011a21703b872d40a801bc1865
Neil Shah <nshah@lcs.on.ca>
2025-02-12 08:47:45 -0500

    Created the initial static layout.

---
+++ StepperExample/Views/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  StepperExample
+//
+ 
+import SwiftUI
+ 
+struct ContentView: View {
+    
+    // MARK: Stored properties
+ 
+    
+    // MARK: Computed properties
+    var body: some View {
+        VStack {
+            
+            Spacer()
+            
+            HStack(alignment: .top) {
+ 
+                Text("5")
+                    .font(.system(size: 96))
+ 
+                Text("2")
+                    .font(.system(size: 44))
+ 
+                Text("=")
+                    .font(.system(size: 96))
+ 
+                Text("25")
+                    .font(.system(size: 96))
+            }
+                        
+            Spacer()
+        }
+        .padding()
+    }
+}
+ 
+#Preview {
+    ContentView()
+}
+ 
